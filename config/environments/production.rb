31d5ccb2552c6114c028eaee0d51df14de4e4fdb
clairezed <clairezuliani@gmail.com>
2017-03-31 15:49:15 +0200

    FIX prod conf

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,15 +93,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { :host => HOST }
   config.action_mailer.asset_host = URL
 
-  # config.action_mailer.delivery_method = :smtp
-  # config.action_mailer.smtp_settings = {
-  #   :address => "smtp.sendgrid.net",
-  #   :port => '587',
-  #   :domain => HOST,
-  #   :authentication => :plain,
-  #   :user_name => "studiohb",
-  #   :password => "90plokij"
-  # }
+
 
   config.middleware.use ExceptionNotification::Rack,
   email: {
